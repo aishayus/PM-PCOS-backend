afeb7ecc672d1ba00077eaa603fb48bb363afed7
aishayus <aishayussuff2005@gmail.com>
2025-06-16 09:24:32 +0100

    thrid commit

--- routes/wearableRoute.js
+++ routes/wearableRoute.js
@@ -3,14 +3,20 @@ import {
     connectWearableDevice, 
     checkWearableConnection, 
     getWearableData,
-    receiveWearableData  
+    receiveWearableData,
+    checkConnectionRequest,
+    confirmConnection
 } from '../controllers/wearableController.js';
 import authMiddleware from './../middlewares/auth.js';
 
 const wearableRouter = express.Router();
+
 wearableRouter.post('/connect', authMiddleware, connectWearableDevice);
 wearableRouter.get('/status', authMiddleware, checkWearableConnection);
 wearableRouter.get('/getdata', authMiddleware, getWearableData);
+
+wearableRouter.get('/check-connection', checkConnectionRequest);
+wearableRouter.post('/confirm-connection', confirmConnection);
 wearableRouter.post('/wearabledata', receiveWearableData);
 
 export default wearableRouter;
